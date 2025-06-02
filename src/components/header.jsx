185c0e5309bc769e579889497af2684977f61f73
steven-halla <steven.r.halla@gmail.com>
2025-06-02 10:42:54 -0700

    black header comopnent

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavBar = styled.nav`
+    background-color: black;
+    padding: 1rem;
+`;
+
+const NavList = styled.ul`
+    display: flex;
+    gap: 1rem;
+`;
+
+const NavItem = styled.li`
+    & a {
+        color: white;
+        &:hover {
+            color: #cbd5e0; /* equivalent to hover:text-gray-300 */
+        }
+    }
+`;
 
 export const Header = () => {
     return (
-        <nav className="bg-black p-4">
-            <ul className="flex space-x-4">
-                <li><Link to="/" className="text-white hover:text-gray-300">Home</Link></li>
-                <li><Link to="/bio" className="text-white hover:text-gray-300">Bio</Link></li>
-                <li><Link to="/community" className="text-white hover:text-gray-300">Community</Link></li>
-                <li><Link to="/connect" className="text-white hover:text-gray-300">Connect</Link></li>
-                <li><Link to="/cope" className="text-white hover:text-gray-300">Cope</Link></li>
-                <li><Link to="/login" className="text-white hover:text-gray-300">Login</Link></li>
-                <li><Link to="/create-account" className="text-white hover:text-gray-300">Create Account</Link></li>
-                <li><Link to="/logout" className="text-white hover:text-gray-300">Logout</Link></li>
-            </ul>
-        </nav>
+        <NavBar>
+            <NavList>
+                <NavItem><Link to="/">Home</Link></NavItem>
+                <NavItem><Link to="/bio">Bio</Link></NavItem>
+                <NavItem><Link to="/community">Community</Link></NavItem>
+                <NavItem><Link to="/connect">Connect</Link></NavItem>
+                <NavItem><Link to="/cope">Cope</Link></NavItem>
+                <NavItem><Link to="/login">Login</Link></NavItem>
+                <NavItem><Link to="/create-account">Create Account</Link></NavItem>
+                <NavItem><Link to="/logout">Logout</Link></NavItem>
+            </NavList>
+        </NavBar>
     )
 }
